8fe6d6ba6bd3be70bb84822edeafc5fbe43d6702
Spaarsh <spaarshthakkar11010@gmail.com>
2026-03-06 15:23:33 +0800

    [UPDATE] Modifying launch script for out-of-the-box usage

--- arena_isaac/scripts/isaac_python.sh
+++ arena_isaac/scripts/isaac_python.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
-echo "running with isaacsim: python.sh $*" 
-~/isaacsim-4.2.0/python.sh "$@"
+
+# The name of your running container
+CONTAINER_NAME="isaac-sim-test"
+
+echo "Forwarding command to Isaac Sim 5.0.0 Container: $@"
+# This ensures Vulkan knows exactly which driver file to use
+# VULKAN_FIX="export VK_ICD_FILENAMES=/etc/vulkan/icd.d/nvidia_icd.json"
+
+# Use docker exec to run the command INSIDE the container
+# -i (interactive) -t (tty) allows you to see the output in real-time
+docker exec -it $CONTAINER_NAME /bin/bash -ic "source ~/.bashrc && export ROS_DOMAIN_ID=1 && /isaac-sim/python.sh $@"
