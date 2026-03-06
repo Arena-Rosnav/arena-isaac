f1922aa5d1174743612e54c3470682650e536f94
Spaarsh <spaarshthakkar11010@gmail.com>
2026-03-06 16:13:37 +0800

    [UPDATE] Adding isaac-sim Dockerfile

--- arena_isaac/scripts/isaac_python.sh
+++ arena_isaac/scripts/isaac_python.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 
 # The name of your running container
-CONTAINER_NAME="isaac-sim-test"
+CONTAINER_NAME="isaac-sim"
 
-echo "Forwarding command to Isaac Sim 5.0.0 Container: $@"
-# This ensures Vulkan knows exactly which driver file to use
-# VULKAN_FIX="export VK_ICD_FILENAMES=/etc/vulkan/icd.d/nvidia_icd.json"
+echo "Forwarding command to Isaac Sim 5.1.0 Container: $@"
 
 # Use docker exec to run the command INSIDE the container
 # -i (interactive) -t (tty) allows you to see the output in real-time
